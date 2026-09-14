5f35df4c903615fbef77d8c17b160d69b88ad981
Al Fajrin Annas Alamsyah <alfajrinannas23@gmail.com>
2026-09-14 10:23:54 +0700

    fix: enable Android guide and download runtime fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './android-fixes.css'
 import App from './App.tsx'
+import { installAndroidDownloadInterceptor } from './lib/android-download-bridge'
+
+installAndroidDownloadInterceptor()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
