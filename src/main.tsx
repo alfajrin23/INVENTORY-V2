1fef459fc237c781f4e6457a9cb4178eee38c6d4
Al Fajrin Annas Alamsyah <alfajrinannas23@gmail.com>
2026-09-16 08:18:52 +0700

    fix: improve scanner and shopping contrast accessibility

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './android-fixes.css'
+import './accessibility-contrast.css'
 import App from './App.tsx'
 import { installAndroidDownloadInterceptor } from './lib/android-download-bridge'
 
